a38d6ca5f872831a6b390d94e686b36593972cce
MokaHiko <cellscommander@gmail.com>
2024-04-12 16:43:01 +0800

    fixed submodule paths

--- vendor/Hurno/Hurno/src/Defines.h
+++
@@ -1,9 +0,0 @@
-#ifdef H_EXPORT
-    #ifdef H_PLATFORM_WIN32
-        #define HAPI __declspec(dllexport)
-    #elif H_PLATFORM_APPLE
-        #define HAPI 
-    #endif
-#else
-    #define HAPI 
-#endif
--- vendor/SPIRVReflect/tests/glsl/buffer_handle_8.glsl
+++
@@ -1,9 +0,0 @@
-#version 450
-#extension GL_EXT_buffer_reference : enable
-
-layout(buffer_reference) buffer T1;
-layout(set = 3, binding = 1, buffer_reference) buffer T1 {
-   layout(offset = 48) T1  c[2]; // stride = 8 for std430, 16 for std140
-} x;
-
-void main() {}
